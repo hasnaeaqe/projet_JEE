86074bedee65db6b3b692c9361b7a1e78132cef1
khadijalam <khadijalamhailik@gmail.com>
2025-11-29 15:01:52 +0100

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/cabinet/medical/GestionCabinetMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.cabinet.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCabinetMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCabinetMedicalApplication.class, args);
+    }
+
+}
